141d2aca8799b086922abdf4e7764e9304802222
Nathan <nathaniel.mendez@upr.edu>
2023-04-23 19:39:20 -0400

    Added login page. Restructured the app.js file.

---
+++ calendar/src/CalendarPage.js
@@ -0,0 +1,180 @@
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DateTimePicker from "react-datetime-picker";
+// import "react-datepicker/dist/react-datepicker.css";
+import "react-clock/dist/Clock.css";
+// import img from "./logo.png"
+import "./CalendarPage.css";
+
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+const events = [{
+    title: "Dentist Appointment",
+    start: new Date(2023,2,12,12,30),
+    end: new Date(2023,2,12,1,30),
+    resource:"Dr Von",
+    notes:"Cant eat before Appointment"
+},
+{
+    title: "Yearly Checkup",
+    start: new Date(2023,2,10,4),
+    end: new Date(2023,2,10,5,30),
+    resource:"Doctor Stevens",
+    notes:""
+
+},
+{
+    title: "CTScan Appointment",
+    start: new Date(2023,2,21,3),
+    end: new Date(2023,2,21,3,30),
+    resource:"Doctor Emily",
+    notes:"No metal items allowed"
+
+},
+{
+    title: "CTScan Evaluation",
+    start: new Date(2023,2,28,9),
+    end: new Date(2023,2,28,10,30),
+    resource:"Dctor Mendez",
+    notes:""
+},]
+
+
+function CalendarPage() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [allEvents, setAllEvents] = useState(events);
+  const [isDetails,setOpenDetails] = useState(false);
+  const [event,setEvent] = useState("");
+
+
+  function Popup() {
+    const [Appointment, setAppointment] = useState({title: "",
+     start: "", end: "", resource: "", notes: ""});
+
+    function handleAddEvent() {
+      setAllEvents([...allEvents, Appointment]);
+    }
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    }
+  
+    return (
+      <div className="popup">
+        <label style={{position:'absolute',top:0,right:240}}>
+          Appointment Details
+        </label>
+        <form onSubmit={handleSubmit}>
+          <div className="input-grid">  
+            <label>
+              Title:
+              <input type="title" value={Appointment.title} 
+              onChange={(e) => setAppointment({...Appointment,title:e.target.value})} />
+            </label>
+            <label>
+              Date:
+              <DateTimePicker type="date" value={Appointment.start} 
+              onChange={(e) => setAppointment({...Appointment,start:e,end:e})} />
+            </label>
+            <label>
+              Doctor:
+              <input type="doctor"value={Appointment.resource} 
+              onChange={(e) => setAppointment({...Appointment,resource:e.target.value})} />
+            </label>
+            <label  style={{position:'absolute',top:90,right:270}}>
+              Notes:
+              <input  style={{position:'absolute',width:210}} value={Appointment.notes} type="notes"  
+              onChange={(e) => setAppointment({...Appointment,notes:e.target.value})} />
+            </label>
+          </div>
+          <button style={{position:'absolute',top:130,right:250}} type="submit" 
+          onClick={handleAddEvent}>Create Appointment</button>
+        </form>
+      </div>
+    );
+  }
+
+  function ViewAppointment() {  
+    return (
+      <div className="popup">
+        <label style={{position:'absolute',top:0,right:240}}>
+          Appointment Details
+        </label>
+        <div className="input-grid">
+          
+          <label>
+            Title:
+            <input type="title" value={event.title}  />
+          </label>
+          <label>
+            Date:
+            <DateTimePicker type="date" value={event.start}  />
+          </label>
+          <label>
+            Doctor:
+            <input type="doctor"value={event.resource}  />
+          </label>
+          <label  style={{position:'absolute',top:90,right:270}}>
+            Notes:
+            <input  style={{position:'absolute',width:210}} value={event.notes} type="notes"   />
+          </label>
+          <button style={{position:'absolute',top:130,right:320}} type="submit" 
+          onClick={toggleViewAppointment}>Close</button>
+
+        </div>
+      </div>
+    );
+  }
+
+  
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const toggleViewAppointment = (e) => {
+    setOpenDetails(!isDetails);
+    setEvent(e);
+  }
+  
+  return (
+      <div className="App">
+        <div style={{ position: 'absolute', top: 50, left: 50 }}>
+        {/* <img  width={150} left="20" height={150} src={img} alt="logo"/> */}
+     </div>
+          <view style={{position:'absolute',right:40}}>
+            <text>Logged in as: <b>One Medical</b></text>
+          </view>
+
+          <h1>DocCalendar</h1>
+          <h3>Patient: Mary Roshvenster</h3>
+
+          <div className="app">
+          <button onClick={togglePopup}>Add Appointment</button>
+          {isOpen && <Popup />}
+        </div>
+              
+
+
+          <Calendar onSelectEvent={(e)=>toggleViewAppointment(e)}  localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+          {isDetails && <ViewAppointment />}
+
+      </div>
+
+  );
+}
+
+export default CalendarPage;
---
+++ calendar/src/LoginPage.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import './LoginPage.css';
+import background from './background.png';
+import { useNavigate } from "react-router-dom";
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [Rusername, setRUsername] = useState('');
+  const [Rpassword, setRPassword] = useState('');
+
+//HardCoded data
+  var userW = "OneMedical"
+  var passwW = "OneMed1234"
+  var userP = "Mary64"
+  var passwordP = "MaryEsCool2"
+
+  const navigate = useNavigate();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log(username)
+    console.log(password)
+    if(username === userW && password === passwW){
+        navigate("/calendar")
+
+    }else if(username === userP && passwordP === password){
+        //Client Calendar
+    }else{
+        alert("Incorrect username/password please try again.")
+    }
+  }
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    //Register logic
+  }
+
+  return (
+    
+    <div className="login-page" style={{backgroundImage: `url(${background})`}}>
+      <label className='docCal'>DocCalendar</label>
+      <form onSubmit={handleLogin} className='form-login'>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <label className='password'>Forgot Password?</label>
+
+        <button type="submit">Login</button>
+      </form>
+      <form onSubmit={handleRegister} className='form-register'>
+        <h3>Register</h3>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" value={Rusername} onChange={(e) => setRUsername(e.target.value)} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" value={Rpassword} onChange={(e) => setRPassword(e.target.value)} required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
